d128c5a25bcc028c8bd0ac84d2f8c87dc629d983
Brian <brian.hardy67@gmail.com>
2022-04-24 14:24:23 -0400

    added login screen and styling. need Auth service

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartmentsComponent } from './features/departments/departments.component';
 import { EmployeesComponent } from './features/employees/employees.component';
+import { LoginComponent } from './features/login/login.component';
 import { ManagersComponent } from './features/managers/managers.component';
 import { MetricsComponent } from './features/metrics/metrics.component';
 import { MiscComponent } from './features/misc/misc.component';
@@ -9,11 +10,12 @@ import { UserProfileComponent } from './features/user-profile/user-profile.compo
 
 const routes: Routes = [
   { path: '', component: UserProfileComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'metrics', component: MetricsComponent },
   { path: 'departments', component: DepartmentsComponent },
   { path: 'managers', component: ManagersComponent },
   { path: 'employees', component: EmployeesComponent },
-  { path: 'misc', component: MiscComponent}
+  { path: 'misc', component: MiscComponent }
 
 ];
 
