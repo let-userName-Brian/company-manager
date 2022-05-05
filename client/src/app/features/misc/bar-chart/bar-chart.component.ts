a66a674b5623ece2e4626c07891a4974f845b6f8
Brian <brian.hardy67@gmail.com>
2022-05-05 09:59:45 -0400

    added two charts to misc screen (dummy data)

--- client/src/app/features/misc/bar-chart/bar-chart.component.ts
+++ client/src/app/features/misc/bar-chart/bar-chart.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-bar-chart',
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.sass']
 })
-export class BarChartComponent implements OnInit {
+export class BarChartComponent implements  AfterViewInit {
+  canvas: any;
+  ctx: any;
+  @ViewChild('myChart') myChart: any;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngAfterViewInit(): void {
+    this.canvas = this.myChart.nativeElement;
+    this.ctx = this.canvas.getContext('2d');
+    new Chart(this.ctx, {
+      type: 'bar',
+      data: {
+        datasets: [{
+          label: 'Managers',
+          data: [50, 42, 21, 12, 11, 10, 30, 10, 22],
+          backgroundColor: 'rgb(95,99,175)',
+          borderColor: 'rgb(25,167,233)',
+          borderWidth: 1
+        }, 
+        {
+          label: 'Employees',
+          data: [10, 12, 11, 10, 30, 42, 50, 33, 21],
+          backgroundColor: 'rgb(255,99,132)',
+          borderColor: 'rgb(255,99,132)',
+          borderWidth: 1
+        }],
+        labels: ['Marketing', 'Finance', 'HR', 'Production', 'Development', 'Quality Management', 'Sales', 'Research', 'Customer Service'],
+      },
+      options: {
+        responsive: false,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            labels: {
+              color: '#00000'
+            }
+          }
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: '#00000'
+            },
+          },
+          y: {
+            ticks: {
+              color: '#00000'
+            }
+          },
+        }
+      }
+    });
+  };
 
 }
--- client/src/app/features/misc/pie-chart/pie-chart.component.ts
+++ client/src/app/features/misc/pie-chart/pie-chart.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
   styleUrls: ['./pie-chart.component.sass']
 })
-export class PieChartComponent implements OnInit {
-
+export class PieChartComponent implements AfterViewInit {
+  canvas: any;
+  ctx: any;
+  labels: string[] = ['Total', 'Min', 'Max', 'Average'];
+  data: any = {
+    labels: this.labels,
+    datasets: [
+      {
+        data: [100, 60, 110, 80],
+        backgroundColor: [
+          'rgb(255, 99, 132, 0.6)',
+          'rgb(54, 162, 235, 0.6)',
+          'rgb(255, 206, 86, 0.6)',
+          'rgb(75, 192, 192, 0.6)',
+        ],
+      }]
+    };
+  @ViewChild('pieChart') myChart: any;
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngAfterViewInit(): void {
+    this.canvas = this.myChart.nativeElement;
+    this.ctx = this.canvas.getContext('2d');
+    new Chart(this.ctx, {
+      type: 'polarArea',
+      data: this.data,
+      options: {
+        maintainAspectRatio: false,
+        responsive: false,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+          },
+        },
+        layout: {
+
+        },
+      },
+    });
+  };
 
 }
